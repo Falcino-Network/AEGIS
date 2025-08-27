1dc3891a3a59cb80dbd24d2e00854eacb4f907a3
AtlantesHellsing <cucu_1025@hotmail.com>
2025-08-27 01:03:55 -0500

    AEGIS-27 Add Docking Area
    C-1: Created Main Tab Pane with some base level logic

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,5 @@ module atlanteshellsing.aegis {
     opens atlanteshellsing.aegis to javafx.fxml;  // Allows FXML reflection
     exports atlanteshellsing.aegis;               // Expose public API
     exports  atlanteshellsing.aegis.gui;
+    exports atlanteshellsing.aegis.components.gui;
 }
